976a2ac4d6dd408f291542a376f9b22f99ba054a
Viet Nguyen <>
2024-04-24 14:46:22 +0700

    done video 2 -refactor code

--- HealthCareViewer/Controllers/SignInViewController.swift
+++ HealthCareViewer/Controllers/SignInViewController.swift
@@ -9,50 +9,76 @@ import UIKit
 
 class SignInViewController: UIViewController {
     
-    let logoImage = UIImageView()
-    let patientCodeLabel = UILabel()
-    let patientCodeTextField = UITextField()
-    let lineView = UIView()
+    private let logoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "LogoBlue")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let patientCodeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Mã số bệnh nhân / bác sĩ"
+        label.textColor = UIColor.blue
+        return label
+    }()
+    
+    private let patientCodeTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Nhập mã số"
+        textField.textColor = UIColor.systemBlue
+        return textField
+    }()
+    
+    private let lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.gray
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
-        logoImage.image = UIImage(named: "LogoBlue")
+        setupViewLogoImageView()
+        setupViewPatientCodeLabel()
+        setupViewPatientTextField()
+        setupViewLineView()
+    }
+    
+    private func setupViewLogoImageView() {
         view.addSubview(logoImage)
-        
         logoImage.snp.makeConstraints { (make) in
             make.top.equalToSuperview().offset(78)
             make.centerX.equalToSuperview()
         }
-        
-        
+    }
+    
+    private func setupViewPatientCodeLabel() {
         view.addSubview(patientCodeLabel)
-        patientCodeLabel.text = "Mã số bệnh nhân / bác sĩ"
-        patientCodeLabel.textColor = UIColor.blue
-        
         patientCodeLabel.snp.makeConstraints { (make) in
             make.left.equalToSuperview().offset(42)
             make.top.equalTo(logoImage.snp.bottom).offset(42)
         }
-        
+    }
+    
+    private func setupViewPatientTextField() {
         view.addSubview(patientCodeTextField)
-        patientCodeTextField.placeholder = "Nhập mã số"
         patientCodeTextField.snp.makeConstraints { (make) in
             make.left.equalTo(patientCodeLabel)
             make.width.equalTo(291)
             make.height.equalTo(35)
             make.top.equalTo(patientCodeLabel.snp.bottom).offset(16)
         }
-        
+    }
+    
+    private func setupViewLineView() {
         view.addSubview(lineView)
-        lineView.backgroundColor = UIColor.gray
         lineView.snp.makeConstraints { (make) in
             make.width.equalTo(patientCodeTextField)
             make.height.equalTo(1)
             make.left.equalTo(patientCodeTextField)
             make.top.equalTo(patientCodeTextField.snp.bottom)
         }
-        
     }
     
 }
